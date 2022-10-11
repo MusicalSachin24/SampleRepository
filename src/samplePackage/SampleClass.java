9036e6ef2a0ad9e47a7c7edd6874656612dfa3f0
Supriya_Sarika <Supriya_Sarika@Suraj>
2022-10-11 15:31:04 +0530

    Sample Project Pushed

---
+++ src/samplePackage/SampleClass.java
@@ -0,0 +1,15 @@
+package samplePackage;
+
+public class SampleClass 
+{
+	public static void main(String[] args)
+	{
+		int a;
+		for(a=1;a<=10;a++)
+		{
+			System.out.println(2*a);
+		}
+		
+	}
+
+}
